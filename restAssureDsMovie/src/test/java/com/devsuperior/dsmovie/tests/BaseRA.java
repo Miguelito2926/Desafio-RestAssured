f32e7cd0b8fec467ddb40a5b6834063fbfad5d13
Miguelito2926 <juniorprol@hotmail.com>
2025-12-28 16:31:24 -0300

    criando configuração

---
+++ restAssureDsMovie/src/test/java/com/devsuperior/dsmovie/tests/BaseRA.java
@@ -0,0 +1,14 @@
+package com.devsuperior.dsmovie.tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseRA {
+//
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+}
