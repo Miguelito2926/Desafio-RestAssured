787c0b910831a1908c746d8da7930a3d3e6bc9d9
Miguelito2926 <juniorprol@hotmail.com>
2025-12-28 17:47:58 -0300

    implementando teste para score

--- restAssureDsMovie/src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ restAssureDsMovie/src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,18 +1,65 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.BaseRA;
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
-public class ScoreControllerRA {
+import static io.restassured.RestAssured.given;
+
+public class ScoreControllerRA extends BaseRA {
 	
 	@Test
-	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {		
+	public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() throws Exception {
+		String token = TokenUtil.obtainAccessToken("alex@gmail.com", "123456");
+
+		JSONObject json = new JSONObject();
+		json.put("movieId", 1000);
+		json.put("score", 4.0);
+
+		given()
+				.header("Authorization", "Bearer " + token)
+				.contentType(ContentType.JSON)
+				.body(json.toString())
+				.when()
+				.put("/scores")
+				.then()
+				.statusCode(404);
 	}
 	
 	@Test
 	public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() throws Exception {
+		String token = TokenUtil.obtainAccessToken("alex@gmail.com", "123456");
+
+		JSONObject json = new JSONObject();
+		json.put("movieId", 1000);
+		json.put("score", 4.0);
+
+		given()
+				.header("Authorization", "Bearer " + token)
+				.contentType(ContentType.JSON)
+				.body(json.toString())
+				.when()
+				.put("/scores")
+				.then()
+				.statusCode(404);
 	}
 	
 	@Test
-	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {		
+	public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() throws Exception {
+		String token = TokenUtil.obtainAccessToken("alex@gmail.com", "123456");
+
+		JSONObject json = new JSONObject();
+		json.put("score", 4.0);
+
+		given()
+				.header("Authorization", "Bearer " + token)
+				.contentType(ContentType.JSON)
+				.body(json.toString())
+				.when()
+				.put("/scores")
+				.then()
+				.statusCode(422);
 	}
 }
--- restAssureDsMovie/src/test/java/com/devsuperior/dsmovie/tests/BaseRA.java
+++ restAssureDsMovie/src/test/java/com/devsuperior/dsmovie/tests/BaseRA.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BaseRA {
-//
+
     @BeforeEach
     void setUp() {
         RestAssured.baseURI = "http://localhost:8080";
